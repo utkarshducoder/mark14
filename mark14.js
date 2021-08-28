2eaeeb5740bce86190bc01caecb995e39f0e934c
utkarshducoder <83503804+utkarshducoder@users.noreply.github.com>
2021-08-28 15:04:54 +0530

    Add files via upload

---
+++ mark14.js
@@ -0,0 +1,39 @@
+var initialPrice = document.querySelector('#initial-price');
+var stocksQuantity = document.querySelector('#stocks-quantity');
+var currentPrice = document.querySelector('#current-price');
+var submitBtn = document.querySelector('#submit-btn');
+var outputBox = document.querySelector('#output-box');
+
+
+function calculateProfitAndLoss(initial,quantity,current){
+    if(initial > current){
+        var loss = (initial - current)*quantity;
+        var lossPercentage = (loss/initial)*100;
+        showOutput(`Hey the loss is ${loss} and the percentage is ${lossPercentage}% `);
+
+    }
+    else if(current > initial){
+        var profit = (current - initial)*quantity;
+        var profitPercentage = (profit/initial)*100;
+        showOutput(`Hey the profit is ${profit} and the percentage is ${profitPercentage}% `);
+    }
+    else{
+        showOutput(`No pain no gain and no gain no pain`);
+
+    }
+}
+
+function submitHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty, curr);
+
+}
+
+function showOutput(message,status){
+    outputBox.innerHTML = message;
+}
+
+submitBtn.addEventListener('click', submitHandler);
